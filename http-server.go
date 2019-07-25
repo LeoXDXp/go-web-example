315bf4584677b4a34f452db83a5850b16b778afb
Leonardo Pizarro <lpizarrobaeza@gmail.com>
2019-07-25 02:28:13 -0400

    ejemplo de json input y output

--- http-server.go
+++ http-server.go
@@ -1,32 +1,27 @@
 // https://gobyexample.com/http-servers
-// Writing a basic HTTP server is easy using the `net/http` package.
+// Writing a basic HTTP server is easy using the `net/http` packedad.
 package main
 
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
 	"log"
-
+	"net/http"
 	//"github.com/hybridgroup/mjpeg"
 	//tf "github.com/tensorflow/tensorflow/tensorflow/go"
 	//"github.com/tensorflow/tensorflow/tensorflow/go/op"
 	//"gocv.io/x/gocv"
 )
 
-// A fundamental concept in `net/http` servers is
-// *handlers*. A handler is an object implementing the
-// `http.Handler` interface. A common way to write
-// a handler is by using the `http.HandlerFunc` adapter
-// on functions with the appropriate signature.
-func hello(w http.ResponseWriter, req *http.Request) {
+type Personaje struct {
+	Nombre   string `json:"nombre"`
+	Apellido string `json:"apellido"`
+	Edad     int    `json:"edad"`
+}
 
-	// Functions serving as handlers take a
-	// `http.ResponseWriter` and a `http.Request` as
-	// arguments. The response writer is used to fill in the
-	// HTTP response. Here out simple response is just
-	// "hello\n".
-	//fmt.Fprintf(w, "hello\n")
+// Descripcion de la funcion, sus parametros de entrada y la salida esperada
+func hello(w http.ResponseWriter, req *http.Request) {
+	// Con navegadores como firefox, al cargar JSON se puede utilizar filtros
 	jsonData, err := json.Marshal("hello")
 	if err != nil {
 		http.Error(w, "500", http.StatusInternalServerError)
@@ -38,31 +33,34 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func headers(w http.ResponseWriter, req *http.Request) {
-
-	// This handler does something a little more
-	// sophisticated by reading all the HTTP request
-	// headers and echoing them into the response body.
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
+// curl -s -XPOST -d'{"nombre":"Pepe","apellido":"Cortisona","edad":40}' http://localhost:8090/decode
+func decode(w http.ResponseWriter, req *http.Request) {
+	var user Personaje
+	json.NewDecoder(req.Body).Decode(&user)
+	fmt.Fprintf(w, "%s %s tiene %d años!", user.Nombre, user.Apellido, user.Edad)
+	log.Printf("Un usuario indica que %s %s tiene %d años!", user.Nombre, user.Apellido, user.Edad)
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Puede revisar /hello o /headers \n")
+	mapData := map[string]map[string]string{"Nuevo Link": map[string]string{"nombre": "/decode", "tipo": "json"},
+		"Antiguo Link": map[string]string{"nombre": "/hello", "tipo": "http"}}
+	jsonData, err := json.Marshal(mapData)
+	if err != nil {
+		http.Error(w, "500", http.StatusInternalServerError)
+	} else {
+		// Set Content-Type header so that clients will know how to read response
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonData)
+	}
+	//fmt.Fprintf(w, "Puede revisar /hello o /headers \n")
 }
 
 func main() {
 
-	// We register our handlers on server routes using the
-	// `http.HandleFunc` convenience function. It sets up
-	// the *default router* in the `net/http` package and
-	// takes a function as an argument.
 	log.Printf("Running")
 	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/decode", decode)
 	http.HandleFunc("/", index)
 
 	// Finally, we call the `ListenAndServe` with the port
