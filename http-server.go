bd5df7b98aa65b16ca6a54b3d855679972344d07
Leonardo Pizarro <lpizarrobaeza@gmail.com>
2019-09-07 17:48:13 -0400

    agregando links para continuar despues

--- http-server.go
+++ http-server.go
@@ -1,15 +1,25 @@
 // https://gobyexample.com/http-servers
+// https://godoc.org/github.com/tensorflow/tensorflow/tensorflow/go#NewGraph
+// https://tutorialedge.net/golang/go-file-upload-tutorial/
+// https://github.com/tensorflow/tensorflow/blob/master/tensorflow/go/example_inception_inference_test.go#L127
 // Writing a basic HTTP server is easy using the `net/http` packedad.
 package main
 
 import (
+	"archive/zip"
+	"bufio"
+	"mime/multipart"
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	//"github.com/hybridgroup/mjpeg"
-	//tf "github.com/tensorflow/tensorflow/tensorflow/go"
-	//"github.com/tensorflow/tensorflow/tensorflow/go/op"
+	tf "github.com/tensorflow/tensorflow/tensorflow/go"
+	"github.com/tensorflow/tensorflow/tensorflow/go/op"
 	//"gocv.io/x/gocv"
 )
 
@@ -53,18 +63,302 @@ func index(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write(jsonData)
 	}
-	//fmt.Fprintf(w, "Puede revisar /hello o /headers \n")
 }
 
+// UploadFile uploads a file to the server
+func UploadFile(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        http.Redirect(w, r, "/", http.StatusSeeOther)
+        return
+    }
+
+    file, handle, err := r.FormFile("file")
+    if err != nil {
+        fmt.Fprintf(w, "%v", err)
+        return
+    }
+    defer file.Close()
+
+    mimeType := handle.Header.Get("Content-Type")
+    switch mimeType {
+    case "image/jpeg":
+        saveFile(w, file, handle)
+
+    case "image/png":
+        saveFile(w, file, handle)
+    default:
+        jsonResponse(w, http.StatusBadRequest, "The format file is not valid.")
+    }
+}
+
+func saveFile(w http.ResponseWriter, file multipart.File, handle *multipart.FileHeader) {
+    data, err := ioutil.ReadAll(file)
+    if err != nil {
+        fmt.Fprintf(w, "%v", err)
+        return
+    }
+
+    err = ioutil.WriteFile("/tmp/"+handle.Filename, data, 0644)
+    if err != nil {
+        fmt.Fprintf(w, "%v", err)
+        return
+    }
+    jsonResponse(w, http.StatusCreated, "File uploaded successfully!.")
+}
+
+func jsonResponse(w http.ResponseWriter, code int, message string) {
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
+    fmt.Fprint(w, message)
+}
+
+
+func tfmodel(w http.ResponseWriter, req *http.Request) {
+	// Run inference on *imageFile.
+	// For multiple images, session.Run() can be called in a loop (and
+	// concurrently). Alternatively, images can be batched since the model
+	// accepts batches of image data as input.
+	tensor, err := makeTensorFromImage("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	output, err := session.Run(
+		map[tf.Output]*tf.Tensor{
+			graph.Operation("input").Output(0): tensor,
+		},
+		[]tf.Output{
+			graph.Operation("output").Output(0),
+		},
+		nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// output[0].Value() is a vector containing probabilities of
+	// labels for each image in the "batch". The batch size was 1.
+	// Find the most probably label index.
+	probabilities := output[0].Value().([][]float32)[0]
+	printBestLabel(probabilities, labelsfile)
+
+	//        mapData := map[string]map[string]string{"Nuevo Link": map[string]string{"nombre": "/decode", "tipo": "json"},
+	//                "Antiguo Link": map[string]string{"nombre": "/hello", "tipo": "http"}}
+	//        jsonData, err := json.Marshal(mapData)
+	if err != nil {
+		http.Error(w, "500", http.StatusInternalServerError)
+	} else {
+		// Set Content-Type header so that clients will know how to read response
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonData)
+	}
+}
+
+var session *tf.Session
+var graph *tf.Graph
+var labelsfile, modelfile string
+
 func main() {
+	log.Printf("loading tf model...")
+	var err error
+	modelfile, labelsfile, err = modelFiles("/opt/ssd_mobilenet_v1_coco_11_06_2017/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	model, err := ioutil.ReadFile(modelfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Construct an in-memory graph from the serialized form.
+	graph = tf.NewGraph()
+	if err := graph.Import(model, ""); err != nil {
+		log.Fatal(err)
+	}
+	// Create a session for inference over graph.
+	session, err = tf.NewSession(graph, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
 
 	log.Printf("Running")
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/decode", decode)
 	http.HandleFunc("/", index)
+	http.HandleFunc("/tf", tfmodel)
 
 	// Finally, we call the `ListenAndServe` with the port
 	// and a handler. `nil` tells it to use the default
 	// router we've just set up.
 	log.Fatal(http.ListenAndServe(":8090", nil))
+}
+
+// Auxiliary functions
+func printBestLabel(probabilities []float32, labelsFile string) {
+	bestIdx := 0
+	for i, p := range probabilities {
+		if p > probabilities[bestIdx] {
+			bestIdx = i
+		}
+	}
+	// Found the best match. Read the string from labelsFile, which
+	// contains one line per label.
+	file, err := os.Open(labelsFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var labels []string
+	for scanner.Scan() {
+		labels = append(labels, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Printf("ERROR: failed to read %s: %v", labelsFile, err)
+	}
+	fmt.Printf("BEST MATCH: (%2.0f%% likely) %s\n", probabilities[bestIdx]*100.0, labels[bestIdx])
+}
+
+// Convert the image in filename to a Tensor suitable as input to the Inception model.
+func makeTensorFromImage(filename string) (*tf.Tensor, error) {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	// DecodeJpeg uses a scalar String-valued tensor as input.
+	tensor, err := tf.NewTensor(string(bytes))
+	if err != nil {
+		return nil, err
+	}
+	// Construct a graph to normalize the image
+	graph, input, output, err := constructGraphToNormalizeImage()
+	if err != nil {
+		return nil, err
+	}
+	// Execute that graph to normalize this one image
+	session, err := tf.NewSession(graph, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer session.Close()
+	normalized, err := session.Run(
+		map[tf.Output]*tf.Tensor{input: tensor},
+		[]tf.Output{output},
+		nil)
+	if err != nil {
+		return nil, err
+	}
+	return normalized[0], nil
+}
+
+// The inception model takes as input the image described by a Tensor in a very
+// specific normalized format (a particular image size, shape of the input tensor,
+// normalized pixel values etc.).
+//
+// This function constructs a graph of TensorFlow operations which takes as
+// input a JPEG-encoded string and returns a tensor suitable as input to the
+// inception model.
+func constructGraphToNormalizeImage() (graph *tf.Graph, input, output tf.Output, err error) {
+	// Some constants specific to the pre-trained model at:
+	// https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
+	//
+	// - The model was trained after with images scaled to 224x224 pixels.
+	// - The colors, represented as R, G, B in 1-byte each were converted to
+	//   float using (value - Mean)/Scale.
+	const (
+		H, W  = 224, 224
+		Mean  = float32(117)
+		Scale = float32(1)
+	)
+	// - input is a String-Tensor, where the string the JPEG-encoded image.
+	// - The inception model takes a 4D tensor of shape
+	//   [BatchSize, Height, Width, Colors=3], where each pixel is
+	//   represented as a triplet of floats
+	// - Apply normalization on each pixel and use ExpandDims to make
+	//   this single image be a "batch" of size 1 for ResizeBilinear.
+	s := op.NewScope()
+	input = op.Placeholder(s, tf.String)
+	output = op.Div(s,
+		op.Sub(s,
+			op.ResizeBilinear(s,
+				op.ExpandDims(s,
+					op.Cast(s,
+						op.DecodeJpeg(s, input, op.DecodeJpegChannels(3)), tf.Float),
+					op.Const(s.SubScope("make_batch"), int32(0))),
+				op.Const(s.SubScope("size"), []int32{H, W})),
+			op.Const(s.SubScope("mean"), Mean)),
+		op.Const(s.SubScope("scale"), Scale))
+	graph, err = s.Finalize()
+	return graph, input, output, err
+}
+
+func modelFiles(dir string) (modelfile, labelsfile string, err error) {
+	const URL = "https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip"
+	var (
+		model   = filepath.Join(dir, "tensorflow_inception_graph.pb")
+		labels  = filepath.Join(dir, "imagenet_comp_graph_label_strings.txt")
+		zipfile = filepath.Join(dir, "inception5h.zip")
+	)
+	if filesExist(model, labels) == nil {
+		return model, labels, nil
+	}
+	log.Println("Did not find model in", dir, "downloading from", URL)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return "", "", err
+	}
+	if err := download(URL, zipfile); err != nil {
+		return "", "", fmt.Errorf("failed to download %v - %v", URL, err)
+	}
+	if err := unzip(dir, zipfile); err != nil {
+		return "", "", fmt.Errorf("failed to extract contents from model archive: %v", err)
+	}
+	os.Remove(zipfile)
+	return model, labels, filesExist(model, labels)
+}
+
+func filesExist(files ...string) error {
+	for _, f := range files {
+		if _, err := os.Stat(f); err != nil {
+			return fmt.Errorf("unable to stat %s: %v", f, err)
+		}
+	}
+	return nil
+}
+
+func download(URL, filename string) error {
+	resp, err := http.Get(URL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = io.Copy(file, resp.Body)
+	return err
+}
+
+func unzip(dir, zipfile string) error {
+	r, err := zip.OpenReader(zipfile)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+	for _, f := range r.File {
+		src, err := f.Open()
+		if err != nil {
+			return err
+		}
+		log.Println("Extracting", f.Name)
+		dst, err := os.OpenFile(filepath.Join(dir, f.Name), os.O_WRONLY|os.O_CREATE, 0644)
+		if err != nil {
+			return err
+		}
+		if _, err := io.Copy(dst, src); err != nil {
+			return err
+		}
+		dst.Close()
+	}
+	return nil
 }
